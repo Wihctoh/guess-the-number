402a0c97307e9a059be52668ec71b2f1cc39fe5e
Wihctoh <rivertoxi911@gmail.com>
2023-03-23 13:05:12 +0300

    add oop style

--- app.js
+++ app.js
@@ -1,45 +1,92 @@
-let randomNumber = Math.round(Math.random() * 100);
+// let randomNumber = Math.round(Math.random() * 100);
 
-function guessTheNumber(randomNumber_) {
-  try {
-    let startNumber = 0;
-    let finishNumber = 100;
+// function guessTheNumber(randomNumber_) {
+//   try {
+//     let startNumber = 0;
+//     let finishNumber = 100;
 
-    for (let i = 0; i < finishNumber; i++) {
-      let inputNumber = prompt(
-        `Программа загадала рандомное число. Ваша задача угадать его. Введите число от ${startNumber} до ${finishNumber}`
-      );
-      if (inputNumber === null){
-        alert('Вы не угадали'); 
-        break;
-      } 
-        
+//     for (let i = 0; i < finishNumber; i++) {
+//       let inputNumber = prompt(
+//         `Программа загадала рандомное число. Ваша задача угадать его. Введите число от ${startNumber} до ${finishNumber}`
+//       );
+//       if (inputNumber === null){
+//         alert('Вы не угадали');
+//         break;
+//       }
 
-      if (inputNumber == randomNumber_) {
-        alert("Вы угадали!");
-        break;
-      } else if (inputNumber > randomNumber_) {
-        if (finishNumber > inputNumber) {
-          finishNumber = inputNumber;
+//       if (inputNumber == randomNumber_) {
+//         alert("Вы угадали!");
+//         break;
+//       } else if (inputNumber > randomNumber_) {
+//         if (finishNumber > inputNumber) {
+//           finishNumber = inputNumber;
+//         }
+
+//         alert(
+//           `Вы ввели число ${inputNumber} и это слишком большое значение. Введите число от ${startNumber} до ${finishNumber}`
+//         );
+//         continue;
+//       } else if (inputNumber < randomNumber_) {
+//         if (startNumber < inputNumber) {
+//           startNumber = inputNumber;
+//         }
+//         alert(
+//           `Вы ввели число ${inputNumber} и это слишком маленькое значение. Введите число от ${startNumber} до ${finishNumber}`
+//         );
+//         continue;
+//       }
+//     }
+//   } catch (error) {
+//     return error.message;
+//   }
+// }
+
+// guessTheNumber(randomNumber);
+
+class Guess {
+  guessTheNumber(randomNumber_) {
+    try {
+      let startNumber = 0;
+      let finishNumber = 100;
+
+      for (let i = 0; i < finishNumber; i++) {
+        let inputNumber = prompt(
+          `Программа загадала рандомное число. Ваша задача угадать его. Введите число от ${startNumber} до ${finishNumber}`
+        );
+        if (inputNumber === null) {
+          alert("Вы не угадали");
+          break;
         }
 
-        alert(
-          `Вы ввели число ${inputNumber} и это слишком большое значение. Введите число от ${startNumber} до ${finishNumber}`
-        );
-        continue;
-      } else if (inputNumber < randomNumber_) {
-        if (startNumber < inputNumber) {
-          startNumber = inputNumber;
+        if (inputNumber == randomNumber_) {
+          alert("Вы угадали!");
+          break;
+        } else if (inputNumber > randomNumber_) {
+          if (finishNumber > inputNumber) {
+            finishNumber = inputNumber;
+          }
+
+          alert(
+            `Вы ввели число ${inputNumber} и это слишком большое значение. Введите число от ${startNumber} до ${finishNumber}`
+          );
+          continue;
+        } else if (inputNumber < randomNumber_) {
+          if (startNumber < inputNumber) {
+            startNumber = inputNumber;
+          }
+          alert(
+            `Вы ввели число ${inputNumber} и это слишком маленькое значение. Введите число от ${startNumber} до ${finishNumber}`
+          );
+          continue;
         }
-        alert(
-          `Вы ввели число ${inputNumber} и это слишком маленькое значение. Введите число от ${startNumber} до ${finishNumber}`
-        );
-        continue;
       }
+    } catch (error) {
+      return error.message;
     }
-  } catch (error) {
-    return error.message;
   }
 }
+randomNumber = Math.round(Math.random() * 100);
 
-guessTheNumber(randomNumber);
+const guess = new Guess();
+
+guess.guessTheNumber(randomNumber);
